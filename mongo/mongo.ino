af7eae1d288014416eb2dfcf7339547cfe01cd16
smart-life-tech <christlightworld@gmail.com>
2023-10-31 20:39:17 +0100

    modified:   mongo/mongo.ino 	modified:   mongoDbEsp.ino

--- mongo/mongo.ino
+++ mongo/mongo.ino
@@ -91,9 +91,9 @@ void POSTData()
   if (WiFi.status() == WL_CONNECTED)
   {
 #if defined(ESP8266)
-    X509List cert("FB:73:80:3E:5D:B0:D9:8D:80:B6:9D:D5:9E:E7:87:4F:C2:48:D9:CA");
+    X509List cert("A8:28:BE:2D:7C:52:F0:DA:4C:BD:8B:99:87:16:32:BF:FA:DF:FD:C7");
     WiFiClientSecure client;
-    client.setFingerprint("FB:73:80:3E:5D:B0:D9:8D:80:B6:9D:D5:9E:E7:87:4F:C2:48:D9:CA");
+    client.setFingerprint("A8:28:BE:2D:7C:52:F0:DA:4C:BD:8B:99:87:16:32:BF:FA:DF:FD:C7");
 #elif defined(ESP32)
     WiFiClientSecure client;
 #endif
